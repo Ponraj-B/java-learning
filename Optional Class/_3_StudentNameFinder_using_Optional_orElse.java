3f6670a51b16ef15b333810379ba9f33fef1ad32
Ponraj_B <ponraj322002@gmail.com>
2025-05-26 16:20:46 +0530

    Update _3_StudentNameFinder_using_Optional_orElse.java

--- Optional Class/_3_StudentNameFinder_using_Optional_orElse.java
+++ Optional Class/_3_StudentNameFinder_using_Optional_orElse.java
@@ -14,7 +14,7 @@ class Main
         stud.put(3, "Kumar");
         stud.put(4, "Jesu");
         int target = 5; // Change target to a non-existing key for demonstration
-        String name = Main.findNameById(target).orElse("not_found"); // orElse method will check the returned value by the optional.of() or Optional.ofNullable() and if the value is null it will provide the static value from the orElse method
+        String name = Main.findNameById(target).orElse("not_found"); // orElse method will check the returned value by the Optional.ofNullable() and if the value is null it will provide the static value from the orElse method
         if(name.equals("not_found"))
         {
             System.out.println("Name not found for the id " + target);
