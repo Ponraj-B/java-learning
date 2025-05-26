f3266365ee7928e7d5037c16b03fba374a5b306d
Ponraj_B <ponraj322002@gmail.com>
2025-05-26 16:19:02 +0530

    Create _4_StudentNameFinder_using_Optional_orElseThrow.java

---
+++ Optional Class/_4_StudentNameFinder_using_Optional_orElseThrow.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Main 
+{
+    static Map<Integer, String> stud = new HashMap<>();
+
+    public static void main(String[] args) 
+    {
+        stud.put(1, "Ponraj");
+        stud.put(2, "Siva");
+        stud.put(3, "Kumar");
+        stud.put(4, "Jesu");
+        stud.put(null,"null");
+        int target = 5; // Change target to a non-existing key for demonstration
+        
+        try
+        {
+            String name = Main.findNameById(target).orElseThrow(() -> new RuntimeException("Value " + target +" is not existing")); // Using OrElseThrow we can throw an Exception or Error if the the returned value from of or ofNullable is null 
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception raised - " + e.getMessage());
+        }
+    }
+
+    public static Optional<String> findNameById(int id) 
+    {
+        if (stud.containsKey(id)) 
+        {
+            return Optional.ofNullable(stud.get(id));
+        }
+        return Optional.empty();
+    }
+}
